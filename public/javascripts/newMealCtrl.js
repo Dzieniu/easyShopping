9f533efc026d980510e363abd7f280bb85fd977c
Dzieniu <dzieniu@gmail.com>
2017-05-22 15:55:20 +0200

    form validation added

--- public/javascripts/newMealCtrl.js
+++ public/javascripts/newMealCtrl.js
@@ -9,6 +9,14 @@ function($scope,$location,auth,$http){
 
 	$scope.addMeal = function(){
 		postData = { name:$scope.newMealName,username:auth.currentUser(),products:$scope.ingredients};
+			if($scope.checkIngredients()==false){
+				swal(
+					  'Coś pominąłeś',
+					  'Sprawdź jeszcze raz formularz dodawania posiłków',
+					  'error'
+					)
+				return;
+			}
 			$http.post("/mealslist", postData).success(function(data2,status) {
 				swal(
 					  'Dodałeś posiłek',
@@ -18,11 +26,20 @@ function($scope,$location,auth,$http){
 			});
 		$scope.ingredients = [{name: "",count: 0,unit: "" }];
 		$scope.newMealName = ""
-	}
+	};
 	$scope.addIngredient = function(){
 		var newItem = $scope.ingredients.length+1;
    		$scope.ingredients.push({name: "", count: 0, unit:""});
-   	}
+   	};
+   	$scope.checkIngredients= function(){
+   		var status=true
+   		$scope.ingredients.forEach( function(element, index) {
+   			if(element.name=="" || !element.count>0 || element.unit==""){
+   				status=false;
+   			}
+   		});
+   		return status;
+   	};
 
 	$scope.showNewMealForm=false
 	
